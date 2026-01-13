e159797ac6571c46915c771214a582179f6fd514
Francisco Sabino <fdasabino@gmail.com>
2026-01-13 12:12:42 +0100

    added enviroment to log in server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = createApp();
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`);
 });
