e1ada0c0825065a1f4acc8d9232ed23e7437dd7e
Francisco Sabino <fdasabino@gmail.com>
2026-01-23 12:54:13 +0100

    chore: update Zod dependency to version 4.3.6 and clean up validate middleware

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 
 // src/middleware/validate.ts
-export const validate =
-  (schema: z.ZodTypeAny) => async (req: Request, res: Response, next: NextFunction) => {
+export const validate = (schema: z.ZodTypeAny) => async (req: Request, res: Response, next: NextFunction) => {
+  void res;
     try {
       // Throws error if invalid
       await schema.parseAsync({
